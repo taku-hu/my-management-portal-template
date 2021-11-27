6a811b1c22b77a41f8092ab5a3ed046970fa81a7
taku-hu <hs.tm.ec.a.tt@gmail.com>
2021-11-28 00:33:42 +0900

    upd: refactor libs

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -5,9 +5,10 @@ import type { NextApiRequest } from 'next'
 export const createAxios = (cookie?: NextApiRequest['headers']['cookie']) => {
   const { create, isAxiosError } = defaultAxios
   const isClientSide = typeof window !== 'undefined'
+  const baseURL = isClientSide ? undefined : process.env.NEXT_PUBLIC_BASE_ENDPOINT
 
   const axios = create({
-    baseURL: isClientSide ? undefined : process.env.NEXT_PUBLIC_BASE_ENDPOINT,
+    baseURL,
     withCredentials: true,
     ...(cookie && {
       headers: {
