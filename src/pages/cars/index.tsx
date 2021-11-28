b54f1c0ed4791794c2348d1778462ba1c49a83b1
taku-hu <hs.tm.ec.a.tt@gmail.com>
2021-11-28 23:39:33 +0900

    add: Create car management page

--- src/pages/cars/index.tsx
+++ src/pages/cars/index.tsx
@@ -1,37 +1,133 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Card, CardContent, CardMedia, Grid, Button } from '@mui/material'
 import { HeaderDrawer } from '@/components/templates/HeaderDrawer'
-import { ReactElement } from 'react'
+
+import type { ReactElement } from 'react'
 
 const Cars = () => {
   return (
     <Box>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-        sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-        eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-        neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-        tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-        sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-        tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-        gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-        tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-        eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-        posuere sollicitudin aliquam ultrices sagittis orci a.
-      </Typography>
+      <Grid container spacing={1} sx={{ p: 1 }}>
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+          <Card square sx={{ display: 'flex' }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  トヨタ セレナ
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  オプションテキスト
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button variant="text" color="success">編集</Button>
+                <Button variant="text" color="error">削除</Button>
+              </Box>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://mui.com/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+          <Card square sx={{ display: 'flex' }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  トヨタ セレナ
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  オプションテキスト
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button variant="text" color="success">編集</Button>
+                <Button variant="text" color="error">削除</Button>
+              </Box>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://mui.com/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+          <Card square sx={{ display: 'flex' }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  トヨタ セレナ
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  オプションテキスト
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button variant="text" color="success">編集</Button>
+                <Button variant="text" color="error">削除</Button>
+              </Box>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://mui.com/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+          <Card square sx={{ display: 'flex' }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  トヨタ セレナ
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  オプションテキスト
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button variant="text" color="success">編集</Button>
+                <Button variant="text" color="error">削除</Button>
+              </Box>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://mui.com/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+          <Card square sx={{ display: 'flex' }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  トヨタ セレナ
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  オプションテキスト
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button variant="text" color="success">編集</Button>
+                <Button variant="text" color="error">削除</Button>
+              </Box>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://mui.com/static/images/cards/live-from-space.jpg"
+              alt="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
